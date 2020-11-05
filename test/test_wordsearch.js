7be63c306877b950b982742bcb7a30adcf08712b
mohdtorabi <mitq8.mt@gmail.com>
2020-11-05 01:30:17 +0000

    pair programming with Kevin & Marcio

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,89 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['E', 'T', 'N', 'E', 'E', 'D', 'P', 'H'],
+      ['I', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'I', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'L', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFIELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['E', 'T', 'N', 'E', 'E', 'D', 'P', 'H'],
+      ['I', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'I', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'L', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
+    ], 'UDUVEEP');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['E', 'T', 'N', 'E', 'E', 'D', 'P', 'H'],
+      ['I', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'I', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'L', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
+    ], 'APARRYAAD');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['E', 'T', 'N', 'E', 'E', 'D', 'P', 'H'],
+      ['I', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'I', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'L', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFIELDS');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['E', 'T', 'N', 'E', 'E', 'D', 'P', 'H'],
+      ['I', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['I', 'F', 'R', 'I', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['L', 'D', 'C', 'L', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'D', 'Q', 'U', 'A', 'L'],
+    ], 'WTFMHFDDZ');
+
+    assert.isFalse(result);
+  });
+
 });
